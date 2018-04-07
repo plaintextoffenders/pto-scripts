361fc230df5f5f88cce703d219a644b112df4523
Aviem Zur <aviemzur@gmail.com>
2018-04-07 08:28:04 +0300

    Change offenders to csv

--- update_offenders.py
+++ update_offenders.py
@@ -5,8 +5,8 @@ import pytumblr
 import re
 from itertools import chain
 
-# Domains filename. Assumes each line contains a single domain.
-DOMAINS_FILENAME = '../plaintextoffenders/offenders.txt'
+# Domains filename. Assumes csv file where each line contains: domain,post_url
+DOMAINS_FILENAME = '../plaintextoffenders/offenders.csv'
 # Tumblr keys filename. Assumes first line is consumer key and second line is consumer secret.
 KEYS_FILENAME = 'tumblr_keys'
 # Limit number of posts to fetch from Tumblr.
@@ -17,11 +17,29 @@ SCROLL = 50
 DOMAINS_REGEX = re.compile('>([^<]*)<')
 DOMAINS_SPLIT_REGEX = re.compile(',\s*')
 
-def get_domains(limit, offset):
-    posts = tumblr_client.posts('plaintextoffenders.com', limit = limit, offset = offset)['posts']
-    return set(chain.from_iterable(extract_domains(post) for post in posts))
+keys = [l.strip() for l in open(KEYS_FILENAME).readlines()]
+existing_domains = set([l.strip().split(',')[0] for l in open(DOMAINS_FILENAME).readlines()])
 
-def extract_domains(post):
+tumblr_client = pytumblr.TumblrRestClient(consumer_key = keys[0], consumer_secret = keys[1])
+
+def get_offenders(limit, offset):
+    posts = tumblr_client.posts('plaintextoffenders.com', limit = limit, offset = offset)['posts']
+    return set(chain.from_iterable(parse_post(post) for post in posts))
+
+def parse_post(post):
+    try:
+        post_url = post['post_url']
+        return map(lambda domain: '{},{}'.format(domain, post_url), get_domains(post))
+    except:
+        print 'Failed to parse post:'
+        if 'caption' in post:
+            print post['caption']
+        else:
+            print 'no caption'
+        print post_url
+        return []
+
+def get_domains(post):
     caption = post['caption']
     domains = DOMAINS_SPLIT_REGEX.split(DOMAINS_REGEX.search(caption).group(1))
     return map(encode_domain, domains)
@@ -29,17 +47,13 @@ def extract_domains(post):
 def encode_domain(str):
     return str.encode('utf-8').strip().replace('\xa0', '').replace('\xc2', '')
 
-def write_domains(potential_domains, existing_domains):
+def write_domains(domains):
     with open(DOMAINS_FILENAME, 'a') as f:
-        for domain in potential_domains - existing_domains:
+        for domain in domains - existing_domains:
             if domain:
                 f.write(domain + '\n')
 
 if __name__ == '__main__':
-    keys = [l.strip() for l in open(KEYS_FILENAME).readlines()]
-    existing_domains = set([l.strip() for l in open(DOMAINS_FILENAME).readlines()])
-    tumblr_client = pytumblr.TumblrRestClient(consumer_key = keys[0], consumer_secret = keys[1])
-
     for i in range(0, LIMIT / SCROLL):
-        domains = get_domains(SCROLL, i * SCROLL)
-        write_domains(domains, existing_domains)
+        domains = get_offenders(SCROLL, i * SCROLL)
+        write_domains(domains)
