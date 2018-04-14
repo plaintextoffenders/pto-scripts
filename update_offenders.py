6a03603383478e2cade418f0812ee81b1be4fb42
Aviem Zur <aviemzur@gmail.com>
2018-04-14 10:26:42 +0300

    Fix offenders csv version

--- update_offenders.py
+++ update_offenders.py
@@ -17,11 +17,6 @@ SCROLL = 50
 DOMAINS_REGEX = re.compile('>([^<]*)<')
 DOMAINS_SPLIT_REGEX = re.compile(',\s*')
 
-keys = [l.strip() for l in open(KEYS_FILENAME).readlines()]
-existing_domains = set([l.strip().split(',')[0] for l in open(DOMAINS_FILENAME).readlines()])
-
-tumblr_client = pytumblr.TumblrRestClient(consumer_key = keys[0], consumer_secret = keys[1])
-
 def get_offenders(limit, offset):
     posts = tumblr_client.posts('plaintextoffenders.com', limit = limit, offset = offset)['posts']
     return set(chain.from_iterable(parse_post(post) for post in posts))
@@ -29,7 +24,7 @@ def get_offenders(limit, offset):
 def parse_post(post):
     try:
         post_url = post['post_url']
-        return map(lambda domain: '{},{}'.format(domain, post_url), get_domains(post))
+        return map(lambda domain: (domain, post_url), get_domains(post))
     except:
         print 'Failed to parse post:'
         if 'caption' in post:
@@ -47,13 +42,20 @@ def get_domains(post):
 def encode_domain(str):
     return str.encode('utf-8').strip().replace('\xa0', '').replace('\xc2', '')
 
-def write_domains(domains):
+def write_domains(offenders, existing_domains):
     with open(DOMAINS_FILENAME, 'a') as f:
-        for domain in domains - existing_domains:
-            if domain:
-                f.write(domain + '\n')
+        for offender in offenders:
+            domain = offender[0]
+            post_url = offender[1]
+            if domain not in existing_domains:
+                f.write('{},{}\n'.format(domain, post_url))
 
 if __name__ == '__main__':
+    keys = [l.strip() for l in open(KEYS_FILENAME).readlines()]
+    existing_domains = set([l.strip().split(',')[0] for l in open(DOMAINS_FILENAME).readlines()])
+
+    tumblr_client = pytumblr.TumblrRestClient(consumer_key = keys[0], consumer_secret = keys[1])
+
     for i in range(0, LIMIT / SCROLL):
-        domains = get_offenders(SCROLL, i * SCROLL)
-        write_domains(domains)
+        offenders = get_offenders(SCROLL, i * SCROLL)
+        write_domains(offenders, existing_domains)
