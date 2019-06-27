7f62e7ca4df09bcce27de6d3a161e48f251a74ef
Alon Reznik <alonreznik@gmail.com>
2019-06-27 20:51:35 +0300

    rewrite_master

--- update_offenders_new.py
+++ update_offenders_new.py
@@ -1,6 +1,5 @@
 import pytumblr
 import re
-import io
 import unicodecsv as csv
 from datetime import datetime
 
@@ -23,8 +22,13 @@ tumblr_client = pytumblr.TumblrRestClient(consumer_key=keys[0], consumer_secret=
 
 
 def get_offenders(limit, offset):
-    posts = tumblr_client.posts('plaintextoffenders.com', limit=limit, offset=offset)['posts']
-    return set([parse_post(post) for post in posts])
+    posts = tumblr_client.posts('plaintextoffenders.com',
+                                limit=limit, offset=offset)
+    all_posts = []
+    for post in posts.get('posts', []):
+        # Parse post returns a list of dicts. Need to be appended into the all_posts list.
+        all_posts += parse_post(post)
+    return all_posts
 
 
 def parse_post(post):
@@ -32,24 +36,23 @@ def parse_post(post):
     :param post: Post from Tumblr
     :return: list of dicts - (domain, post_url, post_date_)
     """
+    # Parse post url and date
     post_url = post.get('post_url')
-    post_date = post.get('post_date')
+    post_date = post.get('date')
     try:
-        if post_date:
-            post_date = datetime.strptime(post_date, '%Y%m%dT%H:%M:%S.%f')
-
+        # Make domains list per post
         post_domains = [{"domain": domain, "post_url": post_url, "post_date": post_date}
                         for domain in get_domains(post)]
 
         return post_domains
-    except:
-        print 'Failed to parse post:'
+
+    except Exception as e:
+        print('Failed to parse post. Error: {}; Post:'.find(str(e)))
         if 'caption' in post:
-            print post['caption']
+            print(post['caption'])
         else:
-            print 'no caption'
-        print post_url
-        return []
+            print('no caption')
+        print(post_url)
 
 
 def get_domains(post):
@@ -59,75 +62,55 @@ def get_domains(post):
             for dm in domains]
 
 
-def encode_domain(dm):
-    return dm.encode('utf-8').strip().replace('\xa0', '').replace('\xc2', '')
-
-
-def read_domains():
-    existing_domains = {}
-    with io.open(DOMAINS_FILENAME, mode="r", encoding="utf-8-sig") as offender_file:
-        of_reader = csv.DictReader(offender_file, delimiter=',', quotechar='"')
-        for row in of_reader:
-            domain = row.get('domain')
-            post_url = row.get('post_url')
-            post_date = datetime.strptime(row.get('post_date'), '%Y-%m-%d %H:%M:%S %Z') if row.get('post_date') \
-                else datetime.utcnow()
-
-            if (domain.lower() not in existing_domains or
-                    existing_domains.get(domain, {}).get('post_date') <= post_date):
-                existing_domains[domain] = {"post_url": post_url, "post_date": post_date}
-
-    return existing_domains
-
-
-def append_domains(offenders, existing_domains, reformed_domains):
-    bChange = False
-    for offender in offenders:
-
-        domain = offender[CSV_COLUMN_DOMAIN]
-        post_url = offender[CSV_COLUMN_POST_URL]
-        post_date = offender[CSV_COLUMN_DATE]
-
-        if domain not in reformed_domains:
-            if domain not in existing_domains or dateLessThan(existing_domains[domain][1], post_date):
-                bChange = True
-                existing_domains[domain] = [post_url, post_date]
-
-    return bChange
-
-
-def write_domains(existing_domains):
-    with io.open(DOMAINS_FILENAME, mode="w", encoding="utf-8-sig") as f:
-        for key in existing_domains:
-            post_url = existing_domains[key][0]
-            post_date = existing_domains[key][1]
-            f.write('{},{},{}\n'.format(key, post_url, post_date).decode("utf-8"))
-
-
 def convert_csv(filename):
-    with open(filename, 'rb') as csv_f:
-        return list(csv.DictReader(csv_f))
+    try:
+        with open(filename, 'r') as csv_f:
+            return list(csv.DictReader(csv_f, fieldnames=['domain', 'post_url', 'post_date']))
+    except IOError:
+        # File doesn't exist
+        return []
 
 
-def dateLessThan(date1, date2):
-    dt1 = dt.strptime(date1, '%Y-%m-%d %H:%M:%S %Z')
-    dt2 = dt.strptime(date2, '%Y-%m-%d %H:%M:%S %Z')
+def date_less_then(date1, date2):
+    dt1 = datetime.strptime(date1, '%Y-%m-%d %H:%M:%S GMT')
+    dt2 = datetime.strptime(date2, '%Y-%m-%d %H:%M:%S GMT')
     return dt1 < dt2
 
 
+def write_domains(offenders, existing_domains, mode='wb'):
+    """ Upsert domains in file, using Existing domains and offenders - the latest between them"""
+    new_domains = []
+
+    existing_domains_dict = {
+        dm.get('domain'): {
+            "post_url": dm.get('post_url'),
+            "post_date": dm.get('post_date') or '1900-01-01 00:00:00 GMT',
+            "domain": dm.get('domain')}
+        for dm in existing_domains
+    }
+
+    for o in offenders:
+        existing_domain = existing_domains_dict.get(o.get('domain'))
+        if not existing_domain:
+            new_domains.append(o)
+        elif date_less_then(existing_domain.get('post_date'), o.get('post_date')):
+            new_domains.append(o)
+        else:
+            new_domains.append(existing_domain)
+
+    with open(DOMAINS_FILENAME, mode=mode) as f:
+        writer_ = csv.DictWriter(f, fieldnames=['domain', 'post_url', 'post_date'])
+        writer_.writerows(new_domains)
+
+
 def run():
-    existing_domains = read_domains()
-    reformed_domains = convert_csv(REFORMED_FILENAME)
-    if
+    existing_domains = convert_csv(DOMAINS_FILENAME)
+    existing_domains += convert_csv(REFORMED_FILENAME)
+
+    for i in range(0, LIMIT / SCROLL):
+        offenders = get_offenders(SCROLL, i * SCROLL)
+        write_domains(offenders, existing_domains, mode='wb' if i == 0 else 'ab')
 
 
 if __name__ == '__main__':
-    existing_domains = read_domains()
-    reformed_domains = first_column_from_csv_file(REFORMED_FILENAME)
-
-    bChange = False
-    for i in range(0, LIMIT / SCROLL):
-        offenders = get_offenders(SCROLL, i * SCROLL)
-        bChange = append_domains(offenders, existing_domains, reformed_domains) or bChange
-    if bChange:
-        write_domains(existing_domains)
+    run()
